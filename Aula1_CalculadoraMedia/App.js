8551876fd21f80c963627f24b951b1fccff82252
geovanna barone <geovanna.barone@gmail.com>
2021-10-04 18:11:26 -0300

    atualizacao do projeto 1, colocando as infos das notas na tela

--- Aula1_CalculadoraMedia/App.js
+++ Aula1_CalculadoraMedia/App.js
@@ -9,17 +9,21 @@ var NotaMédia = (n1 + n2 + n3 + n4)
 var NotaFinal = NotaMédia / 4
 var NotaFixada = NotaFinal.toFixed(0)
 
-console.log("Bem vindo Aluno (a) "+ nome)
-console.log("Sua nota final arredondada foi de " + NotaFixada)
+var elementoBemVindo = document.getElementById("bem-vindo")
+var valorBemVindo = "Bem vindo Aluno (a), " + nome;
+elementoBemVindo.innerHTML = valorBemVindo
 
+var elementoNota = document.getElementById("nota")
+var valorNota = "As suas notas foram, no 1º, 2º, 3º e 4º bimestres respectivamente, " + n1 + " ," + n2 + " ," + n3 + " e " + n4 + ". " + "Sendo assim, sua média foi de " + NotaFixada + ". "
+elementoNota.innerHTML = valorNota
+
+var elementoResultadoNaTela = document.getElementById("resultado");
 if (NotaFixada >= 6) {
-  result = "Sendo assim, você está aprovado, parabéns!";
+  resultado.innerHTML = "Considerando que a média é 6, você está aprovado (a) parabéns!";
 } else {
-  result = "Sendo assim, você está reprovado, sinto muito.";
+  resultado.innerHTML = "Considerando que a média é 6, você está reprovado (a), sinto muito.";
 }
-console.log(result)
-
 
 // Revisão: Variáveis, String, Console.log, to.fixed, operações matemáticas e concatenação (junção de string com variável, com +)
 
-// Desafios: Colocar total geral e média OK / Colocar uma mensagem, de se a pessoa passou ou não OK / mudar estilo da página, ou imagem OK / tentar escrever na página, e não no console FAZER! / fazer conversor de temperaturas ou moedas FAZER! / conta inteira no console.log -> console.log((n1 + n2 + n3 + n4) / 4).toFixed(0)
+// Desafios: Colocar total geral e média OK / Colocar uma mensagem, de se a pessoa passou ou não OK / mudar estilo da página, ou imagem OK / tentar escrever na página, e não no console OK / fazer conversor de temperaturas ou moedas FAZER! / conta inteira no console.log -> console.log((n1 + n2 + n3 + n4) / 4).toFixed(0) // FAZER UM: INSIRA SEU NOME (MATHEUS E GEOVANNA) E AI DEPENDENDO DO NOME QUE COLOCAR, DAI DAR A NOTA.
